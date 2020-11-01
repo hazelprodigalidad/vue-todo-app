1053bc9f67c004d550021e13e98ede8faca9a5b9
hazelprodigalidad <zhellprodigalidad@gmail.com>
2020-11-01 23:36:29 +0800

    first commit

---
+++ precache-manifest.460f5f599b1cdd4a5fe9222d80608400.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd7889b3cdcde0dd0e7",
+    "url": "/css/chunk-49b16e83.a1533543.css"
+  },
+  {
+    "revision": "3967beea879e88747e79",
+    "url": "/css/chunk-vendors.71549ec7.css"
+  },
+  {
+    "revision": "ecdc189605747c676e0b105fcd60b647",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eead54da39dd25d4451",
+    "url": "/js/app.8f8e1519.js"
+  },
+  {
+    "revision": "afa64b77ce66d432d6b7",
+    "url": "/js/chunk-2d22d746.c2619ae2.js"
+  },
+  {
+    "revision": "4bd7889b3cdcde0dd0e7",
+    "url": "/js/chunk-49b16e83.2c0d223e.js"
+  },
+  {
+    "revision": "3967beea879e88747e79",
+    "url": "/js/chunk-vendors.8369149a.js"
+  },
+  {
+    "revision": "b497742f054eed334b04e26b9ca4f41b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
